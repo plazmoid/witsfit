3f429b4b1c4bd6db0394f156f9193e7c26753b9c
plazmoid <kronos44@mail.ru>
2019-06-08 03:00:23 +0500

    proxies list

--- main.py
+++ main.py
@@ -1,19 +1,34 @@
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel
 import sys
+import proxies
 
-class WExample(QWidget):
+__author__ = 'p1azm0id'
+__version__ = '0.0.2'
+
+class WExample(QMainWindow):
     
     def __init__(self):
         super().__init__()
         self.initUI()
         
     def initUI(self):
+        self.btn_proxy = QPushButton("Get proxy", self)
+        self.btn_proxy.move(30, 50)
+        self.btn_proxy.clicked.connect(self.proxiesClicked)
+        
+        self.lbl_proxy = QLabel(self)
+        self.lbl_proxy.move(2, 5)
         self.setGeometry(300, 300, 300, 500)
-        self.setWindowTitle('WITSFIT v 0.0.1')
+        self.setWindowTitle(f'WITSFIT v.{__version__}')
         self.show()
+        
+    def proxiesClicked(self):
+        proxy = proxies.get_working_proxy('https')
+        self.lbl_proxy.setText(proxy)
         
         
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = WExample()
-    sys.exit(app.exec_())
+    app.exec_()
+
---
+++ proxies.py
@@ -0,0 +1,55 @@
+import requests
+import logging
+from random import choice
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+countries = ['AL', 'BS', 'DE', 'EG', 'SE', 'UA', 'US', 'NL', 'FR']
+
+headers = {
+    'User-Agent': 'curl/7.65.0',
+    'Accept': '*/*'
+}
+
+def _get_proxy_list(proxy_type, country=None):
+    assert proxy_type in ['http', 'https'], f'{proxy_type} ещё не поддерживается' #, 'socks4', 'socks5']
+    params = {
+        'type': proxy_type,
+        'anon': 'anonymous',
+        'country': country or choice(countries)
+    }
+    logger.info(f'Country: {params["country"]}')
+    logger.info(f'Type: {params["type"]}')
+    url = 'https://www.proxy-list.download/api/v1/get/'
+    req = requests.get(url, params=params)
+    return req.text
+
+def is_proxy_available(proxy, p_type):
+    logger.info(f'Checking {proxy}')
+    req = requests.get('https://2ip.ru', proxies={p_type: proxy}, headers=headers)
+    result_ip = req.text.strip()
+    #logger.debug(f'{result_ip} == {proxy.split(":")[0]}')
+    if result_ip == proxy.split(':')[0]:
+        return True
+    return False
+
+def _check_proxy_list(proxy_list, ptype):
+    for p in proxy_list:
+        if is_proxy_available(p, ptype):
+            return p
+    else:
+        return False
+
+def get_working_proxy(ptype, **kwargs):
+    while True:
+        plist = _get_proxy_list(ptype, **kwargs).split('\r\n')
+        logger.info(f'Got list: {plist}')
+        while len(plist) > 1:
+            proxy = _check_proxy_list(plist, ptype)
+            if proxy:
+                return proxy
+
+if __name__ == '__main__':
+    #print(is_proxy_available('13.52.154.16:3128', 'https'))
+    print(get_working_proxy('https'))
