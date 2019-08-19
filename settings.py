3e8f46695e1495d2277dae572861346809977bcf
plazmoid <kronos44@mail.ru>
2019-08-20 00:49:56 +0500

    moving to the client-server arch

---
+++ settings.py
@@ -0,0 +1,13 @@
+__author__ = 'p1azm0id'
+__version__ = '0.0.3'
+
+DEFAULT_PORT = 8080
+
+INTRO = f"""
+WITSFIT ({__version__}) - WTF IS IT (obviously i can't come up with a simple name lulz)
+Big helper for the p1azm0id's dumb system
+
+***************************************
+
+"""
+
